4c53befb86830827c17b16ecd59f021572f02bbb
Memory Clutter <memclutter@gmail.com>
2017-12-08 20:04:20 +0300

    Pretty urls

--- config/web.php
+++ config/web.php
@@ -46,14 +46,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
