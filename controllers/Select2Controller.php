ca0e5a6e0a6457490261d4a3e77ea1a9aa4d7fc8
Memory Clutter <memclutter@gmail.com>
2017-12-11 22:15:47 +0300

    Install select2 widget

---
+++ controllers/Select2Controller.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Currency;
+use app\models\Employer;
+use app\models\Location;
+use yii\db\ActiveRecord;
+use yii\rest\Controller;
+
+/**
+ * Select2 Controller.
+ *
+ * @package app\controllers
+ */
+class Select2Controller extends Controller
+{
+    /**
+     * Return currencies list for select2 widgets.
+     *
+     * @param null $query
+     * @param null $id
+     * @return array
+     */
+    public function actionCurrency($query = null, $id = null)
+    {
+        return $this->getList(Currency::className(), $query, $id);
+    }
+
+    /**
+     * Return employers list for select2 widgets.
+     *
+     * @param null $query
+     * @param null $id
+     * @return array
+     */
+    public function actionEmployer($query = null, $id = null)
+    {
+        return $this->getList(Employer::className(), $query, $id);
+    }
+
+    /**
+     * Return locations list for select2 widgets.
+     *
+     * @param null $query
+     * @param null $id
+     * @return array
+     */
+    public function actionLocation($query = null, $id = null)
+    {
+        return $this->getList(Location::className(), $query, $id);
+    }
+
+    /**
+     * Return models list for select2 widgets.
+     *
+     * @param $modelClass
+     * @param null $queryString
+     * @param null $id
+     * @return array
+     */
+    private function getList($modelClass, $queryString = null, $id = null)
+    {
+        if (!empty($queryString)) {
+            $results = $this->getListByQueryString($modelClass, $queryString);
+        } elseif (!empty($id)) {
+            $results = $this->getListSelectedModel($modelClass, $id);
+        } else {
+            $results = $this->getListEmpty($modelClass);
+        }
+
+        return compact('results');
+    }
+
+    /**
+     * Get list by query string.
+     *
+     * @param ActiveRecord $modelClass
+     * @param string $queryString
+     * @return array
+     */
+    private function getListByQueryString($modelClass, $queryString)
+    {
+        return $this->buildQuery($modelClass)
+            ->where(['like', 'title', $queryString])
+            ->all();
+    }
+
+    /**
+     * Get list selected model.
+     *
+     * @param ActiveRecord $modelClass
+     * @param integer $id
+     * @return array
+     */
+    private function getListSelectedModel($modelClass, $id)
+    {
+        return [
+            'id' => $id,
+            'text' => $modelClass::findOne($id)->getAttribute('title'),
+        ];
+    }
+
+    /**
+     * Get list empty
+     *
+     * @param ActiveRecord $modelClass
+     * @return mixed
+     */
+    private function getListEmpty($modelClass)
+    {
+        return $this->buildQuery($modelClass)->all();
+    }
+
+    /**
+     * Build query.
+     *
+     * @param ActiveRecord $modelClass
+     * @return \yii\db\ActiveQuery
+     */
+    private function buildQuery($modelClass)
+    {
+        return $modelClass::find()
+            ->select('id, title as text')
+            ->limit(20)
+            ->orderBy('title')
+            ->asArray();
+    }
+}
--- views/vacancy/_form.php
+++ views/vacancy/_form.php
@@ -1,11 +1,35 @@
 <?php
 
+use app\models\Currency;
+use app\models\Employer;
+use app\models\Location;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\JsExpression;
 use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Vacancy */
 /* @var $form yii\widgets\ActiveForm */
+
+$pluginOptions = [
+    'allowClear' => true,
+    'minimumInputLength' => false,
+    'language' => [
+        'errorLoading' => new JsExpression("function () { return 'Waiting for results...'; }"),
+    ],
+    'ajax' => [
+        'url' => '',
+        'dataType' => 'json',
+        'data' => new JsExpression('function(params) { return {query: params.term}; }')
+    ],
+    'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+    'templateResult' => new JsExpression('function(entity) { return entity.text; }'),
+    'templateSelection' => new JsExpression('function (entity) { return entity.text; }'),
+];
+
 ?>
 
 <div class="vacancy-form">
@@ -16,7 +40,15 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'employer_id')->textInput() ?>
+    <?= $form->field($model, 'employer_id')->widget(Select2::className(), [
+        'initValueText' => empty($model->employer_id) ? '' : Employer::findOne($model->employer_id)->title,
+        'options' => ['placeholder' => $model->getAttributeLabel('employer_id')],
+        'pluginOptions' => ArrayHelper::merge($pluginOptions, [
+            'ajax' => [
+                'url' => Url::to(['/select2/employer']),
+            ],
+        ]),
+    ]) ?>
 
     <?= $form->field($model, 'salary_from')->textInput(['maxlength' => true]) ?>
 
@@ -24,9 +56,25 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'salary_amount')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'salary_currency_id')->textInput() ?>
+    <?= $form->field($model, 'salary_currency_id')->widget(Select2::className(), [
+        'initValueText' => empty($model->salary_currency_id) ? '' : Currency::findOne($model->salary_currency_id)->title,
+        'options' => ['placeholder' => $model->getAttributeLabel('employer_id')],
+        'pluginOptions' => ArrayHelper::merge($pluginOptions, [
+            'ajax' => [
+                'url' => Url::to(['/select2/currency']),
+            ],
+        ])
+    ]) ?>
 
-    <?= $form->field($model, 'location_id')->textInput() ?>
+    <?= $form->field($model, 'location_id')->widget(Select2::className(), [
+        'initValueText' => empty($model->location_id) ? '' : Location::findOne($model->location_id)->title,
+        'options' => ['placeholder' => $model->getAttributeLabel('location_id')],
+        'pluginOptions' => ArrayHelper::merge($pluginOptions, [
+            'ajax' => [
+                'url' => Url::to(['/select2/location']),
+            ],
+        ])
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
